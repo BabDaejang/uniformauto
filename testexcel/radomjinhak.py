39046595623fd6843f97da5df715b1a4dece3655
BabDaejang <sorang84@gmail.com>
2022-08-16 15:58:07 +0900

    원적교, 진학교까지 세팅완료

--- testexcel/radomjinhak.py
+++ testexcel/radomjinhak.py
@@ -1,14 +1,9 @@
 #-*-coding:utf-8 -*-
 
-# import random #랜덤한 데이터를 만들어 보자. 랜덤 데이터를 만들어서 엑셀 형식으로 저장한다.
 # import openpyxl #엑셀을 제어하기 위한 openpyxl 임포트
 
 ## 다시 생각해보니 csv 파일로 일단 만들고 다 만들어진 파일을 엑셀로 변화하는게 낫겠다
 # csv 파일 형식으로 랜덤하게 만든다
-
-# 학년은 동일하다 (반복하여 삽입)
-# 파일이 만들어지는 순서대로 반이 만들어진다
-# 번이 1~25에 다다르면 반이 바뀐다
 
 ### https://www.datasciencemadesimple.com/generate-random-number-in-pandas-python-2/ 이 방법으로 랜덤하게 만들자
 
@@ -17,8 +12,8 @@ import pandas as pd
 import random
 
 fname = "김이박최정강조윤장임한오서신권황안송전홍유고문양손배조" #첫번째 성
-name1 = "명영정광중상재경종병진성제준종승민우예현지동순서은수윤태" #중간 이름
-name2 = "자순숙희미영경은정주연람진서빈지현원은재진호수훈준건호철길일남정형" #마지막 이름
+name1 = "명영정광중상재경종병진성제준종승민우예현지동순서은수윤태원" #중간 이름
+name2 = "자순숙희미영경은정주연람진서빈지현원은재진호수훈준건호철길일남정형석" #마지막 이름
 
 # howmanybanbok = 200 #몇 번 반복할 것인지 정하는 변수 // 학번을 만들었기 때문에 howmanybanbok이 필요 없게 되었다.
 
@@ -55,13 +50,30 @@ namelist = [] # 이름이 기록될 리스트를 하나 만들어주자
 
 for i in range(len(bunho)):  # for  문을 통해 random_name()이라는 함수를 반복하자, 몇 번 할 것인지는 위에 있었다.
     ## 그러나 bunho 만큼만 만들 것이므로 이 함수를 밑으로 빼도록 하겠다.
-    rname = random_name() ## random_name() 이라는 함수를 한다. 근데 결과물을 차곡차곡 쌓고 싶은데? append() 를 써보자.
-    ### append() 함수 문제를 해결하기 위해 append() 함수 안에 직접 넣었다. ~~취소~~
-    ### append() 함수 문제를 해결하기 위해 rname = 이라는 변수를 만들고, 그 변수의 결과물을 추가하게 하였다. ~~실패~~
+    iname = random_name() ## random_name() 이라는 함수를 한다. 근데 결과물을 차곡차곡 쌓고 싶은데? append() 를 써보자.
+    ### append() 함수 문제를 해결하기 위해 append() 함수 안에 직접 넣었다. ~~아님~~ iname = 이라는 변수를 만들고, 그 변수의 결과물을 추가하게 하였다. ~~실패~~
     #### 위의 ###가 문제가 아니라 애당초 fname name1 name2 의 쉼표가 문제라는 걸 확인하였다.
-    namelist.append(rname) # random_name()으로 만들어진 것을 namelist에 추가해 달라는 말이다.
+    namelist.append(iname) # random_name()으로 만들어진 것을 namelist에 추가해 달라는 말이다.
 
-print(namelist, bunho) # 여기까지 잘 되었는지 테스트 하고 git push 하겠다
+# 이제 쉬워졌다. 원적교를 만들어주자
+
+MschoolSource = ['아름중', '고운중', '도담중', '종촌중', '다정중', '새롬중', '두루중', '글벗중', 
+'금호중', '반곡중', '보람중', '부강중', '새뜸중', '어진중', '양지중', '연동중', '연서중'] # 원적교 소스다. 이 리스트를 랜덤하게 뽑아서 bunho 횟수만큼 만들도록 하겠다.
+BeforeSchool = [] # 원적교 리스트를 만들어주자
+
+for i in range(len(bunho)):  # for 문을 통해 함수를 bunho의 갯수만큼 반복하자.
+    BeforeSchool.append(random.choice(MschoolSource)) ## 따로 변수 지정을 안하고 append()함수를 바로 썼다// 잘 작동한다
+
+HschoolSource = ['다정고','도담고','두루고','반곡고','보람고','새롬고','세종고','세과영',
+'국제고','대성고','세여고','예술고','장영실','하이텍','소담고'] # 진학교 소스다
+PromoSchool = [] # 진학교 리스트를 만들어주자
+
+for i in range(len(bunho)):  # for 문을 통해 함수를 bunho의 갯수만큼 반복하자.
+    PromoSchool.append(random.choice(HschoolSource)) ## 따로 변수 지정을 안하고 append()함수를 바로 썼다// 잘 작동한다
+
+print(BeforeSchool, PromoSchool) # 위처럼 코드를 2줄로 줄여도 작동하는지 테스트
+
+  
 
 
 
