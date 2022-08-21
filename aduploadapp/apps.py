2eb2a3ae1c64965c8059052f0450dcb6fd9c08d4
BabDaejang <sorang84@gmail.com>
2022-08-21 12:08:44 +0900

    어드민업로드앱 만들기...url 설정 안되어있다

---
+++ aduploadapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AduploadappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aduploadapp'
---
+++ aduploadapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+# 여기선 데이타베이스를 모델링 하는 곳이다. ... pyxlsx 파이 엑셀로부터 정보를 받은 것을
+# 여기 어딘가 모델에 저장을 해야 할 것이다.
+# class Url(models.Model):
+#     link = models.URLField(max_length=200)
+#     new_link = models.URLField(default="")
+
