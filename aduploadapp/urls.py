cfc67b8998d274fdc3e3dd2efc888775adf7fa38
BabDaejang <sorang84@gmail.com>
2022-08-22 00:35:45 +0900

    https://goodthings4me.tistory.com/ 의 form 관련 애용은 무의미 하다구요

--- aduploadapp/urls.py
+++ aduploadapp/urls.py
@@ -4,10 +4,12 @@ import imp
 from multiprocessing.spawn import import_main_path
 from django.urls import path
 # from .views import  // 이건 조만간 해야 한다.
-from .views import AduploadappListView #일단 AduploadappListView 를 불러온다
+from .views import AduploadappCreateView, AduploadappListView #일단 AduploadappListView 를 불러온다
+
 
 urlpatterns = [
     # as_view() : 클래스형 뷰를 내부적으로 함수형으로 처리한단다.
     # name : 결과 페이지로 보여 줄 템플릿 파일에서 해당 URL을 호출할 때 쓰는 별칭
-    path('',AduploadappListView.as_view(), name='list') #http://127.0.0.1/Aduploadapp/
+    path('',AduploadappListView.as_view(), name='list'), #http://127.0.0.1/Aduploadapp/ list ?
+    path('add/',AduploadappCreateView.as_view(), name='add'), #http://127.0.0.1:8000/Aduploadapp/add
 ]
--- aduploadapp/views.py
+++ aduploadapp/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here. 이제 뷰를 만들어 보자
 from django.views.generic.list import ListView #제너릭 뷰로 만들 것이다, 뷰는 클래스(제너릭)뷰, 함수형 뷰가 있다.
-from .models import Aduploadapp #Aduiploadapp 에 필요한 모델이다.
+from django.views.generic.edit import CreateView #클래스(제너릭)뷰 중 CreateView를 추가한다.
+from .models import Aduploadapp #Aduploadapp 에 필요한 모델이다.
+
 
 class AduploadappListView(ListView): # class Adupload의 ListView 란 뜻으로 만들었다
-    model = Aduploadapp # db에 등록된 Aduploadapp 전체를 반환하여 준다
+    model = Aduploadapp # db에 등록된 Aduploadapp 데이터 전체를 반환하여 준다
+
+class AduploadappCreateView(CreateView): #5행에서 임포트한 CreateView를 받는 클래스를 만든다.
+    model = Aduploadapp # 이걸 만들고 다시 aduploadapp/urls.py 를 수정해준다.
+    fields = ['site_name','url'] #데이터를 입력(추가)하거나 수정 뷰인 경우, 
+                                #대상 필드를 지정해야 한다는 의미 (field=[]추가)
+    template_name_suffix = "_create" #추가해준다.
+
+
+
