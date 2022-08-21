414faf142efb081d5bc6425ae4b99f3a8a315a8a
BabDaejang <sorang84@gmail.com>
2022-08-22 07:59:49 +0900

    urls.py오류수정,이제 템플릿 분리와 확장하기 해야함

--- aduploadapp/urls.py
+++ aduploadapp/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     # name : 결과 페이지로 보여 줄 템플릿 파일에서 해당 URL을 호출할 때 쓰는 별칭
     path('',AduploadappListView.as_view(), name='list'), #http://127.0.0.1/Aduploadapp/ list ?
     path('add/',AduploadappCreateView.as_view(), name='add'), #http://127.0.0.1:8000/Aduploadapp/add
-    path('detail/<int:pk>/'), AduploadappDetailView.as_view(), name='detail'), 
+    path('detail/<int:pk>/', AduploadappDetailView.as_view(), name='detail'), 
     #http://127.0.0.1:8000/Aduploadapp/detail/1 ... <int:pk>를 써서 정수로 입력한 것의 번호가 차곡차곡 쌓이게 함 이제 views.py 를 바꾸자
 ]
